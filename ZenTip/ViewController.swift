608cc90f5f37f67314c69f4546d49227a1c51ec7
A01652612 <A01652612@tec.mx>
2024-08-23 14:37:44 -0600

    Stepper separado

--- ZenTip/ViewController.swift
+++ ZenTip/ViewController.swift
@@ -11,54 +11,60 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var totalAmountTextField: UITextField!
     @IBOutlet weak var partySize: UITextField!
-    @IBOutlet weak var partySizeStepper: UIStepper!
     @IBOutlet weak var tipSegmentedControl: UISegmentedControl!
-    
+    var split: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Configurar el placeholder
         partySize.placeholder = "Party Size"
         
-        // Inicializar el valor del stepper
-        partySizeStepper.value = 0
+        // Inicializar el valor del campo de texto
+        updatePartySize()
+    }
+
+    @IBAction func decrementButtonTapped(_ sender: UIButton) {
+        if split > 1 {  // Asegurarse de que el valor no sea menor que 1
+            split -= 1
+            updatePartySize()
+        }
     }
     
-
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        partySize.text = "\(Int(sender.value))"
+    @IBAction func incrementButtonTapped(_ sender: UIButton) {
+        split += 1  // Incrementar el valor de split
+        updatePartySize()
     }
-
-
+    
+    func updatePartySize() {
+        partySize.text = "\(split)"  // Actualizar el campo de texto con el valor de split
+    }
+    
     @IBAction func didTapButton(_ sender: Any) {
-        // Total Amount and party size
+        // Obtener los valores ingresados
         guard let totalAmount = Double(totalAmountTextField.text ?? "0"),
-              let partySize = Int(partySize.text ?? "1") else {
+              let partySizeValue = Int(partySize.text ?? "1") else {
             return
         }
         
-        // Tip %
+        // Obtener el porcentaje de propina seleccionado
         let percentages = [0.10, 0.15, 0.20]
         var selectedTipPercentage = 0.0
         
         if tipSegmentedControl.selectedSegmentIndex == 3 {
-            selectedTipPercentage = 0.18 //example
+            selectedTipPercentage = 0.18 // Valor por defecto si se selecciona "Otro"
         } else {
             selectedTipPercentage = percentages[tipSegmentedControl.selectedSegmentIndex]
         }
         
-        //Calcule Total with tip
+        // Calcular el total con propina y el total por persona
         let totalWithTip = totalAmount * (1 + selectedTipPercentage)
-        let totalPerPerson = totalWithTip / Double(partySize)
+        let totalPerPerson = totalWithTip / Double(partySizeValue)
         
-        // Pass values to TipCalculatorViewController
-        
+        // Pasar los valores calculados a TipCalculatorViewController
         if let tipCalculatorVC = self.storyboard?.instantiateViewController(withIdentifier: "TipCalculatorViewController") as? TipCalculatorViewController {
                     tipCalculatorVC.totalWithTip = totalWithTip
                     tipCalculatorVC.totalPerPerson = totalPerPerson
                     self.navigationController?.pushViewController(tipCalculatorVC, animated: true)
         }
     }
-
 }
-
